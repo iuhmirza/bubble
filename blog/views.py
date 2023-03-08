2cb83c276dae5f5bfb8288d99b204e3cb7b67deb
Ibtisam Mirza <iuhmirza@gmail.com>
2023-03-08 13:33:26 +0000

    Remove dummy data and use db data

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
 
-posts = [
-    {
-        'author': 'Ibtisam',
-        'content': 'bubble is great',
-        'date': '07/03/2023',
-    },
-    {
-        'author': 'Ibtisam',
-        'content': 'sorry to burst your bubble',
-        'date': '07/03/2023',
-    },
-    {
-        'author': 'Ibtisam',
-        'content': 'we\'re in a bubble',
-        'date': '07/03/2023',
-    }
-]
 
 def index(request):
     context = {
-        'posts': posts
+        'posts': Blog.objects.all()
     }
     return render(request, 'blog/index.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', context={'title': 'About'})
