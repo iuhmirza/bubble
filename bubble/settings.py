055317b91d9d97f6502f9b251433d9c28b955a25
Ibtisam Mirza <iuhmirza@gmail.com>
2023-03-08 15:50:41 +0000

    Add crispy forms to style signup form

--- bubble/settings.py
+++ bubble/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'user.apps.UserConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
