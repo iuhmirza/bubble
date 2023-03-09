539da1bb1bfa23e11c0ae9ea81e6f2bf0e6b399c
Ibtisam Mirza <iuhmirza@gmail.com>
2023-03-09 12:11:11 +0000

    Add sign in and sign out pages

--- user/views.py
+++ user/views.py
@@ -8,8 +8,8 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-index')
+            messages.success(request, f'Thanks for signing up {username}, you can now login!')
+            return redirect('login')
     else:
         form = UserSigninForm()
     context = {
