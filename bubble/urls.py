623bae74c1390cee81a4e787e0d322d421334245
Ibtisam Mirza <iuhmirza@gmail.com>
2023-03-13 19:16:17 +0000

    Add password reset views

--- bubble/urls.py
+++ bubble/urls.py
@@ -25,6 +25,10 @@ urlpatterns = [
     path('signup/', user_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('pw-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='pw-reset'),
+    path('pw-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
+    path('pw-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('pw-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     path('profile/', user_views.profile, name='profile'),
     path('', include('blog.urls'))
 ]
