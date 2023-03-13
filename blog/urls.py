37678b26d790d05f926831484f0c1c9bf629896e
Ibtisam Mirza <iuhmirza@gmail.com>
2023-03-13 13:13:48 +0000

    Add blogs by user view

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogDeleteView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogDeleteView, UserBlogListView
 from . import views
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
     path('blog/new/', BlogCreateView.as_view(), name='blog-create'),
     path('blog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog-delete'),
-    path('about/', views.about, name='blog-about')
+    path('user/<str:username>', UserBlogListView.as_view(), name='user-posts'),
+    path ('about/', views.about, name='blog-about')
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Blog
 
@@ -18,6 +19,16 @@ class BlogListView(ListView):
     ordering = ['-date']
     paginate_by = 5
 
+class UserBlogListView(ListView):
+    model = Blog
+    template_name = 'blog/user_blog.html'
+    context_object_name = 'blogs'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Blog.objects.order_by('-date').filter(author=user)
+
 
 class BlogDetailView(DetailView):
     model = Blog
@@ -32,7 +43,7 @@ class BlogCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Blog
     success_url = '/'
 
